d45a1f5b2b95ad5cce7bc638181873bc11faf831
Luciano dii Souza <lucianodiisouza@hotmail.com>
2021-05-10 09:25:49 -0300

    reviewing class componennts > migrate it to fn comps.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import logo from './logo.svg';
+// import { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+	const [reverse, setReverse] = useState(false);
+	const reverseClass = reverse && 'reverse';
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
+				<img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+				<button type="button" onClick={() => setReverse(!reverse)}>
+					Reverse {reverseClass}
+				</button>
 			</header>
 		</div>
 	);
 }
+
+// class App extends Component {
+// 	state = {
+// 		reverse: true,
+// 	};
+
+// 	render() {
+// 		const { reverse } = this.state;
+// 		const reverseClass = reverse && 'reverse';
+
+// 		return (
+// <div className="App">
+// <header className="App-header">
+// <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+// <button type="button" onClick={() => this.setState({ reverse: !reverse })}>
+//	Reverse {reverseClass}
+// 	</button>
+// </header>
+// </div>
+
+// 		);
+// 	}
+// }
 
 export default App;
